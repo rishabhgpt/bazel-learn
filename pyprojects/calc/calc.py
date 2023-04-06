ac6a045fc02709cc92537da46764dc586cd2f231
Rishabh Gupta <rishabhgpt3@gmail.com>
2023-04-06 18:32:42 +0530

    source files

---
+++ pyprojects/calc/calc.py
@@ -0,0 +1,2 @@
+class Calc :
+    def add(self, x, y): return x+y
---
+++ pyprojects/calc/calc_test.py
@@ -0,0 +1,12 @@
+import unittest
+
+from pyprojects.calc.calc import Calc
+
+class TestSum(unittest.TestCase):
+    def test_sum(self):
+        calc = Calc()
+        res = calc.add(1,3)
+        self.assertEqual(res, 4, "should be equal")
+
+if __name__=='__main__':
+    unittest.main()
