d26b4c2030a5fc31ed41c93523634cdae87fe174
Rishabh Gupta <rishabhgpt3@gmail.com>
2023-04-08 21:41:43 +0530

    restructure code

---
+++ src/bookstore/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	"github.com/rishabhgpt/projects/bookstore/repository"
+)
+
+func YourHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Received request")
+	w.Write([]byte(repository.HelloWorld() + " Bazoku is cool!!"))
+}
+
+func getPort() string {
+	port := os.Getenv("PORT")
+	if port == "" {
+		return "8080"
+	}
+
+	return port
+}
+
+func main() {
+	r := mux.NewRouter()
+	// Routes consist of a path and a handler function.
+	r.HandleFunc("/", YourHandler)
+	// Bind to a port and pass our router in
+	port := getPort()
+	log.Println("Going to listen on port: " + port)
+	log.Fatal(http.ListenAndServe(":"+port, r))
+}
